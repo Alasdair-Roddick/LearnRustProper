b51b29c5bfe18f1d1909c8985cf3367022a058ac
Alasdair-Roddick <64404140+Alasdair-Roddick@users.noreply.github.com>
2026-01-11 18:35:45 +1030

    added gitignore

---
+++ lesson-03/target/debug/deps/lesson_03-0a7d8ff9d16627eb.d
@@ -0,0 +1,5 @@
+/Users/alasdairroddick/Development/LearnRust/lesson-03/target/debug/deps/lesson_03-0a7d8ff9d16627eb.d: src/main.rs
+
+/Users/alasdairroddick/Development/LearnRust/lesson-03/target/debug/deps/liblesson_03-0a7d8ff9d16627eb.rmeta: src/main.rs
+
+src/main.rs:
---
+++ lesson-03/target/debug/lesson-03.d
@@ -0,0 +1,1 @@
+/Users/alasdairroddick/Development/LearnRust/lesson-03/target/debug/lesson-03: /Users/alasdairroddick/Development/LearnRust/lesson-03/src/main.rs
